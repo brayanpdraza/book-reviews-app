09a0c5dea13a94c3a23c18e06cd96ec31b38b4cf
Brayan Alejandro Pedraza Leon <brayan_pedraza@javeriana.edu.co>
2025-01-23 11:13:32 -0500

    primer paso sin el backend y sin el github pipeline

---
+++ backend/tests/IntegrationTests/AdaptadorApiTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace AdaptadorApiTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ backend/tests/UnitTests/AplicacionTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace AplicacionTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
