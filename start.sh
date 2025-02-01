0aa064170f19e1b282be153a3a861e4f32876d4a
Brayan Alejandro Pedraza Leon <brayan_pedraza@javeriana.edu.co>
2025-02-01 13:27:58 -0500

    gadgas

--- start.sh
+++ start.sh
@@ -6,6 +6,9 @@ cd backend/src
 export PATH="$PATH:$HOME/.dotnet/tools"
 dotnet tool install --global dotnet-ef --version 6.0.36 || true
 
+echo "=== Restaurando dependencias ==="
+dotnet restore AdaptadorPostgreSQL/AdaptadorPostgreSQL.csproj
+
 echo "=== Directorio actual ==="
 pwd
 
@@ -15,8 +18,7 @@ ls -la
 echo "=== Aplicando migraciones ==="
 dotnet ef database update \
   --project ./AdaptadorPostgreSQL/AdaptadorPostgreSQL.csproj \
-  --startup-project ./AdaptadorAPI/AdaptadorAPI.csproj \
-  --msbuildprojectextensionspath ./obj
+  --startup-project ./AdaptadorAPI/AdaptadorAPI.csproj
 
 echo "=== Compilando API ==="
 dotnet publish -c Release -o ./publish AdaptadorAPI/AdaptadorAPI.csproj
