6893392689575ace120ff1b08e9ccc5ac09671ea
Brayan Alejandro Pedraza Leon <brayan_pedraza@javeriana.edu.co>
2025-02-01 11:04:18 -0500

    gsgsd

--- start.sh
+++ start.sh
@@ -1,3 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+set -e
+
+
+dotnet tool install --global dotnet-ef || true
+
+
+export PATH="$PATH:$HOME/.dotnet/tools"
+
+echo "Ejecutando migraciones de EF..."
 dotnet ef database update --project ./AdaptadorPostgreSQL/AdaptadorPostgreSQL.csproj --startup-project ./out/API.dll
+
+echo "Iniciando la API..."
 dotnet ./out/API.dll
