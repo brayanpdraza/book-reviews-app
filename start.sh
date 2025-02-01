6b1604b59aa4b8a41e17df541d1f4c1e4e876027
Brayan Alejandro Pedraza Leon <brayan_pedraza@javeriana.edu.co>
2025-02-01 11:24:36 -0500

    gsdgsd

--- start.sh
+++ start.sh
@@ -2,10 +2,12 @@
 set -e
 
 dotnet tool install --global dotnet-ef --version 6.0.36 || true
-export PATH="$PATH:$HOME/.dotnet/tools"
+export PATH="$PATH:/root/.dotnet/tools"
 
 echo "Ejecutando migraciones de EF..."
-dotnet ef database update --project ./AdaptadorPostgreSQL/AdaptadorPostgreSQL.csproj --startup-project ./out/API.dll
+dotnet ef database update \
+  --project /app/AdaptadorPostgreSQL/AdaptadorPostgreSQL.csproj \
+  --startup-project /app/API/API.csproj
 
 echo "Iniciando la API..."
-dotnet ./out/API.dll
+dotnet /app/out/API.dll
