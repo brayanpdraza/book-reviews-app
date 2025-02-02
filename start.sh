435500cc6076b36b2a3356e9390b0379d58d0f3e
Brayan Alejandro Pedraza Leon <brayan_pedraza@javeriana.edu.co>
2025-02-01 21:03:39 -0500

    gsdgghs

--- start.sh
+++ start.sh
@@ -6,21 +6,11 @@ cd backend/src
 export PATH="$PATH:$HOME/.dotnet/tools"
 dotnet tool install --global dotnet-ef --version 6.0.36 || true
 
-
 echo "=== Restaurando dependencias ==="
 dotnet restore AdaptadorPostgreSQL/AdaptadorPostgreSQL.csproj
 
-echo "=== Directorio actual ==="
-pwd
-
-echo "=== Contenido de backend/src ==="
-ls -la
-
-echo "=== Aplicando migraciones ==="
-dotnet ef migrations add InitialCreate \
-  --project ./AdaptadorPostgreSQL/AdaptadorPostgreSQL.csproj \
-  --startup-project ./AdaptadorAPI/AdaptadorAPI.csproj
-
+echo "=== Aplicando migraciones pendientes ==="
+dotnet ef database update --project AdaptadorPostgreSQL/AdaptadorPostgreSQL.csproj --startup-project AdaptadorAPI/AdaptadorAPI.csproj
 
 echo "=== Compilando API ==="
 dotnet publish -c Release -o ./publish AdaptadorAPI/AdaptadorAPI.csproj
