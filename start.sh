83e21db202f59fee864166e281a62baaf7b572b8
Brayan Alejandro Pedraza Leon <brayan_pedraza@javeriana.edu.co>
2025-02-02 01:01:47 -0500

    afsa

--- start.sh
+++ start.sh
@@ -9,20 +9,12 @@ dotnet tool install --global dotnet-ef --version 6.0.36 || true
 echo "=== Restaurando dependencias ==="
 dotnet restore AdaptadorPostgreSQL/AdaptadorPostgreSQL.csproj
 
-echo "=== Verificando migraciones ==="
-MIGRATIONS=$(dotnet ef migrations list --project ./AdaptadorPostgreSQL/AdaptadorPostgreSQL.csproj --startup-project ./AdaptadorAPI/AdaptadorAPI.csproj)
-
-if [ -z "$MIGRATIONS" ]; then
-  echo "⚠️ No hay migraciones. Creando InitialCreate..."
-  dotnet ef migrations add InitialCreate --ignore-changes \
-    --project ./AdaptadorPostgreSQL/AdaptadorPostgreSQL.csproj \
-    --startup-project ./AdaptadorAPI/AdaptadorAPI.csproj
-fi
 
 echo "=== Aplicando migraciones ==="
 dotnet ef database update \
   --project ./AdaptadorPostgreSQL/AdaptadorPostgreSQL.csproj \
   --startup-project ./AdaptadorAPI/AdaptadorAPI.csproj
+
 
 echo "=== Compilando API ==="
 dotnet publish -c Release -o ./publish AdaptadorAPI/AdaptadorAPI.csproj
